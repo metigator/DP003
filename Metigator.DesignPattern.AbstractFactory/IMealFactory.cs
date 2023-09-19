823aa881dabbe409cf2969965dc149dcd10b6e3b
Ahmed Mostafa <a.mostafa4687@futuresnet.net>
2023-09-19 14:43:50 +0300

    Using meals factory in ImealFactory

--- Metigator.DesignPattern.AbstractFactory/IMealFactory.cs
+++ Metigator.DesignPattern.AbstractFactory/IMealFactory.cs
@@ -11,17 +11,17 @@
     {
         public IAppetizer PrepareAppetizer()
         {
-            return new PotatoBite("Small", "200-250", 05.99m, new() { "Potato", "Cheese", "Breadcrumbs", "Egg" });
+            return new PotatoBiteFactory().CreateDish();
         }
 
         public IMainCourse PrepareMainCourse()
         {
-            return new Lasagna("Large", "300-600", 14.99m, new() { "Pasta", "Cheese", "Tomato", "Beef" });
+            return new LasagnaFactory().CreateDish();
         }
 
         public IDessert PrepareDessert()
         {
-            return new Tiramisu("Small", "400-600", 08.99m, new() { "Ladyfingers", "Coffee", "Egg yolks", "Mascarpone cheese", "Cocoa powder" });
+            return new TiramisuFactory().CreateDish();  
         }
     }
 
@@ -29,18 +29,18 @@
     {
         public IAppetizer PrepareAppetizer()
         {
-            return new ChickenSalad("Small", "350-450", 08.99m, new() { "Chicken", "Lettuce", "Tomatoes", "Cucumbers", "Salad dressing" });
+            return new ChickenSaladFactory().CreateDish();
         }
 
         public IMainCourse PrepareMainCourse()
         {
-            return new GrilledChicken("Large", "200-300", 15.99m, new() { "Chicken", "Salt", "Pepper", "Paprika" });
+            return new GrilledChickenFactory().CreateDish();
 
         }
 
         public IDessert PrepareDessert()
         {
-            return new FruitSalad("Medium", "100-150", 07.99m, new() { "Apple", "Banana", "Orange", "Berries" });
+            return new FruitSaladFactory().CreateDish();
         }
     }
 }
